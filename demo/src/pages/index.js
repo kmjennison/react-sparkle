1e824e448ca9d6ae6bb49aaf295989a183c2c754
Kevin Jennison <kevin.jennison1@gmail.com>
2020-11-11 14:43:30 -0800

    Set up some demo styles

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -1,5 +1,24 @@
-import React from "react"
+import React from 'react'
+import Sparkles from 'react-sparkle'
+import clsx from 'clsx'
+import indexStyles from "./index.module.css"
 
 export default function Home() {
-  return <div>Hello there!</div>
+  return (
+    <div>
+      <div className={indexStyles.section}>
+        <h2 className={indexStyles.exampleTitle}>Default</h2>
+        <div className={indexStyles.exampleContainer}>
+          <div className={indexStyles.code}>
+            stuff here
+          </div>
+          <div className={clsx(indexStyles.darkBackground, indexStyles.demo)}>
+            <div className={indexStyles.sparkleTarget}>
+              <Sparkles />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
