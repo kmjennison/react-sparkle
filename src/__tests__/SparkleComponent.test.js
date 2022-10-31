0a167c0c14343c0b8b71e2071ed5f71987388c5d
Kevin Jennison <kevin.jennison1@gmail.com>
2022-10-31 16:04:36 -0400

    Switch from Enzyme to RTL; upgrade React

--- src/__tests__/SparkleComponent.test.js
+++ src/__tests__/SparkleComponent.test.js
@@ -1,25 +1,10 @@
 import React from 'react'
-import { mount, shallow } from 'enzyme'
+import { render } from '@testing-library/react'
 
 describe('SparkleComponent', () => {
   // eslint-disable-next-line jest/expect-expect
-  it('mounts without error', () => {
+  it('renders without error', () => {
     const Sparkle = require('../SparkleComponent').default
-    mount(<Sparkle />)
-  })
-
-  it('logs a warning but does not throw an error if the HTML canvas ref is null', () => {
-    const Sparkle = require('../SparkleComponent').default
-
-    const mockConsoleWarn = jest
-      .spyOn(console, 'warn')
-      .mockImplementationOnce(() => {})
-
-    // Shallow rendering means the canvas ref will be null
-    shallow(<Sparkle />)
-
-    expect(mockConsoleWarn).toHaveBeenCalledWith(
-      'No sparkles today :( The canvas did not render.'
-    )
+    render(<Sparkle />)
   })
 })
